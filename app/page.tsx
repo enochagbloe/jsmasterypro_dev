6191e77ad7d944e4c86da125b80ed3e039b24b72
Enoch <enochagbloe16@gmail.com>
2025-01-17 03:14:24 +0000

    tailwind css styles and theming

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
       <div>
-        <h1 className={'text-3xl text-violet-700 font-black'}>Welcome to the World of Next Js </h1>
+        <h1 className='h1-bold text-primary-500'>Welcome to the World of Next Js </h1>
       </div>
   );
 }
