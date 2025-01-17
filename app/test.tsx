5865de3e25a232839dd47dd8a41f8f8d8741c15f
Enoch <enochagbloe16@gmail.com>
2025-01-17 03:12:36 +0000

    Add app/globals.css file

---
+++ app/test.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const test = () => {
+  return (
+    <div>
+      <h1 className='text-3xl text-violet-700 font-black'> Welcome to NextJs </h1>
+    </div>
+  )
+}
+
+export default test
+
